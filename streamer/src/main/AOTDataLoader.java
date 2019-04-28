eb235102b0ad9f2a711be87e3ae6fc9997cdd39c
tengdj <tengdj@vip.qq.com>
2019-04-28 16:48:08 +0800

    clean the code

--- streamer/src/main/AOTDataLoader.java
+++ streamer/src/main/AOTDataLoader.java
@@ -1,7 +1,10 @@
 package main;
 
-public class DataStreamer{
+public class AOTDataLoader{
 
+	/*
+	 * an entry point to start a client to load and send data
+	 * */
     public static void main(String args[]){
 
     	Test.test_create_stream_aotdata();
--- streamer/src/main/Main.java
+++ streamer/src/main/Main.java
@@ -2,9 +2,12 @@ package main;
 
 public class Main {
 	
+    /*
+     * main entry for tests
+     */
 	public static void main(String args[]) {
 		
-		Test.test_flink_streamer_aotdata();
+		Test.test_load_climatedata();
 		//Test.test_streaming_aotdata();
 		//Test.test_geohash();
 
