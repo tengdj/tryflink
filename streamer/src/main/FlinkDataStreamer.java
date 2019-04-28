72a1ba3ebb8d4757fb8f70f09933df32e5c4b361
tengdj <tendj@vip.qq.com>
2019-04-28 16:52:55 +0800

    minor update

---
+++ streamer/src/main/FlinkDataStreamer.java
@@ -0,0 +1,15 @@
+package main;
+
+public class FlinkDataStreamer {
+	/*
+	 * an entry point to start a Flink stream server
+	 * to receive data from a client
+	 * */
+	public static void main(String args[]) {
+		
+		Test.test_flink_streamer_aotdata();
+		//Test.test_streaming_aotdata();
+		//Test.test_geohash();
+
+	}
+}
